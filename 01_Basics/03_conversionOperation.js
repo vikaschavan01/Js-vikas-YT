b6233f6dc20a0bd3750744349ab2a8edb860e3fa
Chavan Vikas Sunil <129142579+vikaschavan01@users.noreply.github.com>
2024-08-22 11:41:46 +0000

    Conversion of Datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "Viku"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" = 33
+// "33abc" = NaN
+// true = 1 / false = 0
+
+let isLoggedIn = "viku"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 = true
+// "" = false
+// "viku" = true
+
+let someNumber = 31
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
