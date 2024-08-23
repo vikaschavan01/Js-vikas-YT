7bfbf476b0d3d41f58b24a5ebe0e53bcca9d511d
Chavan Vikas Sunil <129142579+vikaschavan01@users.noreply.github.com>
2024-08-23 10:27:17 +0000

    Comparisons

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1); // true
+// console.log("02" > 1); // true
+
+// console.log(null > 0); // False
+// console.log(null == 0); // False
+// console.log(null >= 0); // True
+
+// console.log(undefined == 0); // false
+// console.log(undefined < 0); // false
+// console.log(undefined > 0); // false
+
+// ===
+
+console.log("2" === 2); // false
