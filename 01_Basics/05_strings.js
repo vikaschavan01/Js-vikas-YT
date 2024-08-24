6057a9b5fd9d7f1a8d3c1d37765d5872b8f63b35
Chavan Vikas Sunil <129142579+vikaschavan01@users.noreply.github.com>
2024-08-24 06:08:10 +0000

    Stringss

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,33 @@
+const name = "Vikas"
+const repoCount = 20
+
+console.log(`Hello my name is ${name} and my repositories are ${repoCount}.`);
+
+const gameName = new String ('Vikas-SC')
+
+// console.log(gameName[0]);
+
+// console.log(gameName.length);
+
+// console.log(gameName.toUpperCase());
+
+//console.log(gameName);
+
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('S'));
+// console.log(gameName.indexOf('s'));
+
+const newString = gameName.substring(0, 4)
+const anotherString = gameName.slice(-8, 4)
+
+// console.log(newString);
+// console.log(anotherString);
+
+const newStringOne = "            Viku               "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://vikas0123//owrrjfnfvvn%20jdeid"
+// console.log(url.replace('%20', '-'));
+
+console.log(gameName.split('-'));
--- 01_Basics/datatypes_summary.js
+++ 01_Basics/datatypes_summary.js
@@ -18,13 +18,13 @@ const anotherId = Symbol('123')
 
 const bigNumber = 65659421135545654789652145n
 //=============================================
-console.log(typeof score); // number
-console.log(typeof scoreValue); // number 
-console.log(typeof isLoggedIn); // boolean
-console.log(typeof outsideTemp); // Object
-console.log(typeof id); // symbol
-console.log(typeof anotherId); // symbol
-console.log(typeof bigNumber); // bigint
+// console.log(typeof score); // number
+// console.log(typeof scoreValue); // number 
+// console.log(typeof isLoggedIn); // boolean
+// console.log(typeof outsideTemp); // Object
+// console.log(typeof id); // symbol
+// console.log(typeof anotherId); // symbol
+// console.log(typeof bigNumber); // bigint
 
 
 
@@ -44,6 +44,34 @@ const myFunction = function(){
     
 }
 //================================
-console.log(typeof warrior); // Object
-console.log(typeof myObj); // Object 
-console.log(typeof myFunction); // function
+// console.log(typeof warrior); // Object
+// console.log(typeof myObj); // Object 
+// console.log(typeof myFunction); // function
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack Memory (Primitive)
+
+let myGmaiName = "Vikaschavan0812"
+
+let anotherName = myGmaiName
+
+anotherName = "ChavanBhai"
+
+// console.log(myGmaiName);
+// console.log(anotherName);
+//=========================================
+
+// Heap Memory (Non-Primitive)
+
+let userOne = {
+    email : "vikas234@gmailcom",
+    upi : "vikas@ybl" 
+}
+
+let userTwo = userOne
+
+userTwo.email = "akash@gmail.com"
+
+// console.log(userOne.email);
+// console.log(userTwo.email); // Both Output is same = akash@gmail.com.
