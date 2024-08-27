1697278a27ea06160b1920f9c69b132d96f277dd
Chavan Vikas Sunil <129142579+vikaschavan01@users.noreply.github.com>
2024-08-27 16:11:00 +0000

    Numbers

---
+++ 01_Basics/06_nums_maths.js
@@ -0,0 +1,27 @@
+const score = 400
+//console.log(score);
+
+
+const balance = new Number(100)
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 36.889
+
+// console.log(otherNumber.toPrecision(2));
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+console.log(Math.floor(Math.random))
